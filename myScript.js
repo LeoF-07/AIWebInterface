ac3249e36e2ff53212ff7bd8d92287f04e99a177
LeoF-07 <fortin.leonardo07@gmail.com>
2025-10-17 15:41:23 +0200

    First commit

---
+++ myScript.js
@@ -0,0 +1,29 @@
+function onLoad_Setup(){}
+
+async function inviaDomanda() {
+    let domanda = document.getElementById('domanda').value;
+
+    const risposta = document.getElementById('risposta');
+    risposta.innerHTML += `<div class="domanda">${domanda}</div><br>`;
+    risposta.scrollTop = risposta.scrollHeight;
+
+    document.getElementById('domanda').value = "";
+
+
+    domanda = {"domanda": domanda};
+    console.log(domanda);
+
+    const options = {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(domanda)
+    };
+    
+    await fetch("http://127.0.0.1:9000/ask", options).then(
+        response => response.json()
+    ).then((data) => {
+        console.log(data);
+        risposta.innerHTML += `<div class="risposta">${data.content}</div><br>`;
+        risposta.scrollTop = risposta.scrollHeight;
+    }).catch(error => console.log("Si è verificato un errore!"))
+}
